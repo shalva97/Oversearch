aaa22689aa2cfd0a80838a67fdd7da36fb9c2056
shalva97 <shalva258@gmail.com>
2024-05-04 14:31:09 +0400

    add my own library to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ kapt {
 
 //noinspection UseTomlInstead
 dependencies {
+    implementation("io.github.shalva97.overwatch_player_search_api:library:1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.10")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.10")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 pluginManagement {
     repositories {
         google {
@@ -16,9 +19,21 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/shalva97/overwatch-player-search-api")
+            credentials {
+                val properties = Properties().apply {
+                    runCatching { load(FileInputStream("local.properties")) }
+                        .onFailure {
+                            println("Failed to load properties. You need to authenticate with Github Packages to download all dependencies")
+                        }
+                }
+                username = properties["gpr.user"] as String? ?: System.getenv("USERNAME")
+                password = properties["gpr.key"] as String? ?: System.getenv("TOKEN")
+            }
+        }
     }
 }
 
 rootProject.name = "Oversearch"
 include(":app")
- 
