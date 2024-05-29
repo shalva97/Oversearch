7a830de2b64c9c87c4530d9c6e52cf855b54757d
shalva97 <shalva258@gmail.com>
2024-05-29 16:54:08 +0400

    add contraint layout for stats screen

--- app/src/main/java/com/example/oversearch/presentation/screens/stats/PlayerStatsScreen.kt
+++ app/src/main/java/com/example/oversearch/presentation/screens/stats/PlayerStatsScreen.kt
@@ -1,18 +1,24 @@
 package com.example.oversearch.presentation.screens.stats
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.example.oversearch.R
@@ -46,11 +52,32 @@ fun PlayerStatsScreen(
             }
 
             is PlayerStatsState.Statistics -> {
-                AsyncImage(
-                    model = state.profile.player.backgroundImage,
-                    contentDescription = null,
-                    placeholder = painterResource(id = R.drawable.img_sample_player_background)
-                )
+                ConstraintLayout {
+                    val (image, profileImage) = createRefs()
+                    AsyncImage(
+                        modifier = Modifier.constrainAs(image) {
+                            top.linkTo(parent.top)
+                        },
+                        model = state.profile.player.backgroundImage,
+                        contentDescription = null,
+                        placeholder = painterResource(id = R.drawable.img_sample_player_background)
+                    )
+                    AsyncImage(
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                            .size(64.dp)
+                            .constrainAs(profileImage) {
+                                start.linkTo(image.start)
+                                top.linkTo(image.bottom)
+                                bottom.linkTo(image.bottom)
+                            },
+                        model = state.profile.player.image,
+                        contentDescription = null,
+                        placeholder = painterResource(
+                            id = R.drawable.img_sample_player
+                        )
+                    )
+                }
             }
         }
     }
